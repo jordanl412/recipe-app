fb4974849cff7de63f48d733f7562c25c4967011
jordanl412 <jordanlazan@gmail.com>
2023-09-11 15:42:48 -0400

    Removed secret_key and debug=true

--- src_recipe_project/recipe_project/settings.py
+++ src_recipe_project/recipe_project/settings.py
@@ -19,11 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wut2upmubs%679yjv+9%u=nfp@%+f-p&6gk$q-35d!+f@!5+5v'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
